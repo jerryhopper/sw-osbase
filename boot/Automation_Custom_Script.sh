7a3e2035d058676e053952f7164e5b96772fb9cd
JerryHopper <hopper.jerry@gmail.com>
2020-09-30 14:42:33 +0200

    Update Automation_Custom_Script.sh

--- boot/Automation_Custom_Script.sh
+++ boot/Automation_Custom_Script.sh
@@ -2,7 +2,7 @@
 # -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
 # We do not want users to end up with a partially working install, so we exit the script
 # instead of continuing the installation with something broken
-#set -e
+set -e
 #SCRIPT_FILENAME="Automation_Custom_Script"
 #echo "Automation_Custom_Script.sh has started">>/boot/log.txt
 
