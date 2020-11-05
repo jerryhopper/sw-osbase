78c37b9e2f54c08bdd274a572c0e45a0cb3c887a
JerryHopper <hopper.jerry@gmail.com>
2020-11-06 00:26:37 +0100

    removed swoole for speed in builds
    temporary

--- customize-image.sh
+++ customize-image.sh
@@ -101,16 +101,20 @@ InstallPreRequisites(){
 	# echo "root:password" | chpasswd
 
 	#/usr/lib/armbian/armbian-firstrun
+
+
+
 	# SWOOLE
-	log "Cloning and compiling swoole"
-	git clone https://github.com/swoole/swoole-src.git && cd swoole-src
-	git checkout v4.5.5
+	#log "Cloning and compiling swoole"
+	#git clone https://github.com/swoole/swoole-src.git && cd swoole-src
+	#git checkout v4.5.5
+	#phpize && ./configure --enable-sockets --enable-openssl && make && make install
+	#log "Installing swoole"
+	#echo "extension=swoole.so" >> $(php -i | grep php.ini|grep Loaded | awk '{print $5}')
 
-	phpize && ./configure --enable-sockets --enable-openssl && make && make install
 
-	log "Installing swoole"
-	echo "extension=swoole.so" >> $(php -i | grep php.ini|grep Loaded | awk '{print $5}')
-	
+
+
 	log  "Remove unneccesary files"
 	cd .. && rm -rf ./swoole-src
 
