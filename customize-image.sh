a16b1d9ce967ec3e1616e66c4392e566c2fd6422
JerryHopper <hopper.jerry@gmail.com>
2020-11-23 23:53:03 +0100

    Update customize-image.sh

--- customize-image.sh
+++ customize-image.sh
@@ -100,7 +100,7 @@ InstallSwoole(){
 InstallPreRequisites(){
 	#
 	apt-get update
-	apt-get install -y docker docker.io build-essential g++ avahi-daemon avahi-utils libsodium23 libzip5 libedit2 libxslt1.1 nmap curl jq wget git unzip sqlite3 php-dev
+	apt-get install -y docker docker.io build-essential g++ gawk avahi-daemon avahi-utils libsodium23 libzip5 libedit2 libxslt1.1 nmap curl jq wget git unzip sqlite3 php-dev
 
 	# remove new user prompt
 	rm /root/.not_logged_in_yet
